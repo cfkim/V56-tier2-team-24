d86394cdd9f4b5dd0ed67e2268bd68af079731fb
Hyun Woo Kim <hynwkm@gmail.com>
2025-07-28 20:15:19 -0400

    adjust nav items depending on user role

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import type { Role } from "./types/Role";
 
 function App() {
+    const [role, setRole] = useState<Role | undefined>("admin");
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
     return (
         <BrowserRouter>
-            <Header />
+            <Header role={role} isLoggedIn={isLoggedIn} />
             <main>
                 <Routes>
                     <Route path="/" element={<Home />} />
