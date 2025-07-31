5fdbec7bd60acbcb9e071a9a11982d8ab95ec8e3
cfkim <christine.fth.kim@gmail.com>
2025-07-31 13:29:01 -0400

    Complete merge changes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import { Route, Routes, useNavigate } from "react-router-dom";
-import Footer from "./components/footer";
-import Hero from "./components/hero";
-import Login from "./pages/Login";
-import Account from "./pages/Account";
-import { setNavigate } from "./lib/navigation";
-function App() {
-  const navigate = useNavigate()
-  setNavigate(navigate)
-  return (
-    <Routes>
-      <Route path="/" element={<Hero />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/user" element={<Account />}></Route>
-    </Routes>
-  )
-=======
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -38,7 +20,6 @@ function App() {
             <Footer />
         </BrowserRouter>
     );
->>>>>>> 8c8715c8266f7a30c2d45ab54d9954254776f357
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
@@ -11,19 +10,8 @@ import queryClient from "./config/queryClient.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
         <App />
         <ReactQueryDevtools initialIsOpen={false} />
-      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
-=======
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
->>>>>>> 8c8715c8266f7a30c2d45ab54d9954254776f357
 );
