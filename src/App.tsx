41c19e7b45f5903640fafbc7df9c253930a6075c
TheClaireLiu <cliu202211@gmail.com>
2025-08-04 16:03:47 -0400

    Remove Login page and update routing - Delete Login.tsx component - Update App.tsx to remove Login routes - Set ForgotPassword as default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // src/App.tsx
 import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetLinkSent from "./pages/ResetLinkSent";
 import Footer from "./components/footer";
@@ -10,10 +9,9 @@ function App() {
     <div className="min-h-screen flex flex-col">
       <div className="flex-1 flex flex-col">
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/password/forgot" element={<ForgotPassword />} />
           <Route path="/password/reset-link-sent" element={<ResetLinkSent />} />
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<ForgotPassword />} />
         </Routes>
       </div>
       <Footer />
