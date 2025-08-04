3053017e3bdc3aa20fa06cfa5a9b1402c2da6f35
Hyun Woo Kim <hynwkm@gmail.com>
2025-08-04 11:27:52 -0400

    feat: add env example files

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -8,10 +8,16 @@ export interface UserDocument extends mongoose.Document {
     role: "surgeon" | "admin";
     createdAt: Date;
     updatedAt: Date;
-
 }
 
-const userSchema = new mongoose.Schema<UserDocument>({email: {type: String, unique: true, required: true}, password: {type:String, required:true}, role: {type: String, required:true}}, {timestamps: true});
+const userSchema = new mongoose.Schema<UserDocument>(
+    {
+        email: { type: String, unique: true, required: true },
+        password: { type: String, required: true },
+        role: { type: String, required: true },
+    },
+    { timestamps: true }
+);
 
 const UserModel = mongoose.model<UserDocument>("User", userSchema);
 
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
 import UserModel from "../models/user.model";
-import { clearAuthCookies, getAccessTokenCookieOptions, setAuthCookies } from "../utils/cookies";
+import {
+    clearAuthCookies,
+    getAccessTokenCookieOptions,
+    setAuthCookies,
+} from "../utils/cookies";
 
 // const express = require("express");
 // const app = express();
@@ -16,113 +20,120 @@ const authRoutes = Router();
 // This is a simple in-memory store for refresh tokens
 let refreshTokens: string[] = [];
 
-function generateAccessToken(user:any) {
-  return jwt.sign(user, JWT_SECRET, { expiresIn: "15s" });
+function generateAccessToken(user: any) {
+    return jwt.sign(user, JWT_SECRET, { expiresIn: "15s" });
 }
 
 // -- LOGIN HANDLER --
 authRoutes.post("/login", async (req, res) => {
-  const inputEmail = req.body.email;
-  const inputPassword = req.body.password;
-  const rememberMe = req.body.rememberMe;
+    const inputEmail = req.body.email;
+    const inputPassword = req.body.password;
+    const rememberMe = req.body.rememberMe;
 
-  // finds user from email
-  const user = await UserModel.findOne({ email: inputEmail });
+    // finds user from email
+    const user = await UserModel.findOne({ email: inputEmail });
 
-  // if user does not exist, returns error
-  if (user == null) {
-    return res.status(401).send("Cannot find user");
-  }
-
-  try {
-    // compares password with hashed password
-    if (await bcrypt.compare(inputPassword, user.password)) {
-      // create session (tokens)
-      const session_user = { email: inputEmail, id: user._id };
-      const accessToken = generateAccessToken(session_user);
-      const refreshToken = jwt.sign(session_user, JWT_REFRESH_SECRET, { expiresIn: "30d" })
-      
-      refreshTokens.push(refreshToken);
-
-      setAuthCookies({res, accessToken, refreshToken, rememberMe})
-      
-      res.status(200).send({ user, accessToken, refreshToken });
-      
-      
-
-    } else {
-      // if password does not match, returns error
-      res.status(401).send("invalid password");
+    // if user does not exist, returns error
+    if (user == null) {
+        return res.status(401).send("Cannot find user");
     }
-  } catch {
-    // if compare fails
-    res.status(500).send("auth failed");
-  }
+
+    try {
+        // compares password with hashed password
+        if (await bcrypt.compare(inputPassword, user.password)) {
+            // create session (tokens)
+            const session_user = { email: inputEmail, id: user._id };
+            const accessToken = generateAccessToken(session_user);
+            const refreshToken = jwt.sign(session_user, JWT_REFRESH_SECRET, {
+                expiresIn: "30d",
+            });
+
+            refreshTokens.push(refreshToken);
+
+            setAuthCookies({ res, accessToken, refreshToken, rememberMe });
+
+            res.status(200).send({ user, accessToken, refreshToken });
+        } else {
+            // if password does not match, returns error
+            res.status(401).send("invalid password");
+        }
+    } catch {
+        // if compare fails
+        res.status(500).send("auth failed");
+    }
 });
 
 // -- REGISTER HANDLER --
 authRoutes.post("/register", async (req, res) => {
-  try {
-    // Gets email and password from request body and hashes password
-    const inputEmail = req.body.email;
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    try {
+        // Gets email and password from request body and hashes password
+        const inputEmail = req.body.email;
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-    // Checks if user exists in db
-    const existingUser = await UserModel.exists({ email: inputEmail });
-    if(existingUser) {
-      res.status(500).json({ message: "User already exists" });
+        // Checks if user exists in db
+        const existingUser = await UserModel.exists({ email: inputEmail });
+        if (existingUser) {
+            res.status(500).json({ message: "User already exists" });
+        }
+
+        // Creates user in db
+        const user = await UserModel.create({
+            email: inputEmail,
+            password: hashedPassword,
+            role: req.body.role,
+        });
+
+        // Creates session (tokens)
+        const session_user = { id: user._id, email: user.email };
+        const accessToken = generateAccessToken(session_user);
+        const refreshToken = jwt.sign(session_user, JWT_REFRESH_SECRET, {
+            expiresIn: "30d",
+        });
+
+        // "Saves" refresh tokens in memory for testing
+        // TODO: implement tokens with http cookies instead of local storage
+        refreshTokens.push(refreshToken);
+
+        // Returns the access token and refresh token
+        // 201 means created
+        setAuthCookies({ res, accessToken, refreshToken });
+
+        res.status(201).json({
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+        });
+    } catch {
+        res.status(500).send();
     }
-    
-    // Creates user in db
-    const user = await UserModel.create({ email: inputEmail, password: hashedPassword, role: req.body.role });
-
-    // Creates session (tokens)
-    const session_user = { id: user._id, email: user.email};
-    const accessToken = generateAccessToken(session_user);
-    const refreshToken = jwt.sign(session_user, JWT_REFRESH_SECRET, { expiresIn: "30d" });
-    
-    // "Saves" refresh tokens in memory for testing
-    // TODO: implement tokens with http cookies instead of local storage
-    refreshTokens.push(refreshToken);
-
-    // Returns the access token and refresh token
-    // 201 means created
-    setAuthCookies({res, accessToken, refreshToken})
-
-    res.status(201).json({ accessToken: accessToken, refreshToken: refreshToken });
-
-
-  } catch {
-    res.status(500).send();
-  }
 });
 
 // -- REFRESH TOKEN HANDLER --
 authRoutes.get("/refresh", async (req, res) => {
-  const cookieToken = req.cookies.refreshToken 
-  const authHeader = req.headers['authorization'];
-  const refreshToken = authHeader && authHeader.split(' ')[1];
-  if(cookieToken == null) return res.sendStatus(401); // No token provided
-  if (!refreshTokens.includes(cookieToken)) return res.sendStatus(403); // Invalid token
-  jwt.verify(refreshToken, JWT_REFRESH_SECRET, (err:any, user:any) => {
-    
-    if(err) return res.sendStatus(403); // Invalid token
-    const session_user = { id: user.id, email: user.email};
-    const accessToken = generateAccessToken(session_user);
-    res.cookie("accessToken", accessToken, getAccessTokenCookieOptions()).json({ accessToken: accessToken});
-  })
+    const cookieToken = req.cookies.refreshToken;
+    const authHeader = req.headers["authorization"];
+    const refreshToken = authHeader && authHeader.split(" ")[1];
+    if (cookieToken == null) return res.sendStatus(401); // No token provided
+    if (!refreshTokens.includes(cookieToken)) return res.sendStatus(403); // Invalid token
+    jwt.verify(refreshToken, JWT_REFRESH_SECRET, (err: any, user: any) => {
+        if (err) return res.sendStatus(403); // Invalid token
+        const session_user = { id: user.id, email: user.email };
+        const accessToken = generateAccessToken(session_user);
+        res.cookie(
+            "accessToken",
+            accessToken,
+            getAccessTokenCookieOptions()
+        ).json({ accessToken: accessToken });
+    });
 });
 
 // -- LOGOUT HANDLER --
 authRoutes.get("/logout", async (req, res) => {
-  // Removes the refresh token to effectively log out the user
-  const authHeader = req.headers['authorization'];
-  const refreshToken = authHeader && authHeader.split(' ')[1];
-  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
-  clearAuthCookies(res)
-  res.sendStatus(204);
-
-  
+    // Removes the refresh token to effectively log out the user
+    const authHeader = req.headers["authorization"];
+    const refreshToken = authHeader && authHeader.split(" ")[1];
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    clearAuthCookies(res);
+    res.sendStatus(204);
 });
 
 export default authRoutes;
